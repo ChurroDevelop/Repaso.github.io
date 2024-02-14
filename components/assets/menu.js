25d9aa791ae92f18ca1df3f13185881132bba38e
Jhon Carreño <jhonfredy2325@gmail.com>
2024-02-14 13:27:01 -0500

    Se arreglo el js del menu

--- components/assets/menu.js
+++ components/assets/menu.js
@@ -1,5 +1,34 @@
 const cabeceraIcon = document.querySelector(".cabecera--icon");
 const cabeceraLista = document.querySelector(".cabecera--lista");
+
+
+
+const cabeceraInicio = document.querySelector(".siu")
+const cabeceraComunidad = document.querySelector(".nou");
+const cabeceraPlanes = document.querySelector(".que");
+const cabeceraApp = document.querySelector(".so");
+
+
+
+const bars = document.querySelector("#bars");
+// const cabeceraList = document.querySelector("#cabecera--list");
+
 cabeceraIcon.addEventListener("click", ()=>{
     cabeceraLista.classList.toggle("aparecer");
+});
+
+cabeceraInicio.addEventListener("click", ()=>{
+    cabeceraLista.classList.remove("aparecer");
+});
+
+cabeceraComunidad.addEventListener("click", ()=>{
+    cabeceraLista.classList.remove("aparecer");
+})
+
+cabeceraPlanes.addEventListener("click", ()=>{
+    cabeceraLista.classList.remove("aparecer");
+})
+
+cabeceraApp.addEventListener("click", ()=>{
+    cabeceraLista.classList.remove("aparecer");
 });
