17fb10b1a823f106fca84bdcf8ad08de9922f626
JhonFredyCarreñoPintoAdso <jhonfredy2325@gmail.com>
2024-02-15 13:50:35 -0500

    Se configuro bien el js

--- components/assets/menu.js
+++ components/assets/menu.js
@@ -1,17 +1,10 @@
 const cabeceraIcon = document.querySelector(".cabecera--icon");
 const cabeceraLista = document.querySelector(".cabecera--lista");
 
-
-
 const cabeceraInicio = document.querySelector(".siu")
 const cabeceraComunidad = document.querySelector(".nou");
 const cabeceraPlanes = document.querySelector(".que");
 const cabeceraApp = document.querySelector(".so");
-
-
-
-const bars = document.querySelector("#bars");
-// const cabeceraList = document.querySelector("#cabecera--list");
 
 cabeceraIcon.addEventListener("click", ()=>{
     cabeceraLista.classList.toggle("aparecer");
